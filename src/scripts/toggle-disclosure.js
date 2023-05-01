dcf8ab294c0a1829e0a3da7c093d99b496f066e6
arpit-agr <arpit.agrawal2502@gmail.com>
2023-05-01 11:35:13 +0530

    add product category, update gallery

--- src/scripts/toggle-disclosure.js
+++ src/scripts/toggle-disclosure.js
@@ -1,16 +1,16 @@
 function toggleDisclosure() {
 	if (this.getAttribute("aria-expanded") == "false") {
-		this.innerText = " Read Less";
+		this.innerText = " View Less";
 		this.setAttribute("aria-expanded", "true");
 		this.previousElementSibling.setAttribute("data-state", "visible");
 	} else {
-		this.innerText = " Read More";
+		this.innerText = " View More";
 		this.setAttribute("aria-expanded", "false");
 		this.previousElementSibling.setAttribute("data-state", "hidden");
 	}
 }
 const disclosureWidget = document.querySelectorAll(".disclosure-widget");
-const readMoreButton = document.querySelectorAll(".read-more");
+const viewMoreButton = document.querySelectorAll(".view-more");
 
 disclosureWidget.forEach((el) => el.setAttribute("data-state", "hidden"));
-readMoreButton.forEach((el) => el.addEventListener("click", toggleDisclosure));
+viewMoreButton.forEach((el) => el.addEventListener("click", toggleDisclosure));
